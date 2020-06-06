c790b91ea54abc3ae55aa736354c1b6f559d2ad9
Saumalya Sarkar <potter@Saumalyas-MacBook-Air.local>
2020-06-06 19:02:39 +0530

    Configured network-`Mjolnir-Bifrost` for `Mjolnir` project.

---
+++ TERRAFORM/MJOLNIR_NETWORK(MIDGARD)_SETUP/provider.tf
@@ -0,0 +1,24 @@
+# Declare terraform version
+terraform {
+  required_version = ">= 0.12, < 0.13"
+}
+
+# Declare provider
+provider "aws" {
+  region = var.region
+
+  # Allow any 2.x version of the AWS provider
+  version = "~> 2.0"
+}
+
+# Declare remote state file
+terraform {
+    backend "s3" {
+        encrypt = true
+        bucket = "mjolnir-cicd-terraform-remote-state-location"
+        dynamodb_table = "mjolnir-cicd-terraform-state-lock-dynamo-collection"
+        region = "us-east-1"
+        key = "CICD/TERRAFORM/MJOLNIR_NETWORK_MIDGUARD_SETUP/mjolnir_network_midguard_setup.tfstate"
+    }
+}
+
---
+++ TERRAFORM/MJOLNIR_NETWORK(MIDGARD)_SETUP/variables.tf
@@ -0,0 +1,54 @@
+variable "project" {
+    description = "Parent project name"
+    type        = string
+    default     = "mjolnir"
+}
+
+variable "user" {
+    description = "Owner of the services"
+    type        = string
+    default     = "mjolnir_saumalya"
+}
+
+variable "region" {
+    description = "AWS region to be used"
+    type        = string
+    default     = "us-east-1"
+}
+
+variable "remote_state_location_bucket" {
+    description = "Name of the bucket to keep all .tfstate files"
+    type        = string
+    default     = "mjolnir-cicd-terraform-remote-state-location"
+}
+
+variable "remote_state_lock_table" {
+    description = "DynamoDB table name to enforce .tfstate files"
+    type        = string
+    default     = "mjolnir-cicd-terraform-state-lock-dynamo-collection"
+}
+
+variable "vpc_cidr" {
+	default = "75.57.0.0/22"
+    type        = string
+}
+
+variable "public_subnets_cidr" {
+	type = list
+	default = ["75.57.0.0/24", "75.57.1.0/24"]
+}
+
+variable "private_subnets_cidr" {
+	type = list
+	default = ["75.57.2.0/24", "75.57.3.0/24"]
+}
+
+variable "azs" {
+	type = list
+	default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "whitelisted_ips" {
+	type = list
+	default = ["103.218.170.40/32", "42.110.129.206/32"]
+}
